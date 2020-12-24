5905f8eb3bc63d6a475e794120585d4472fa5448
vitov <vitovt.kublitsky@mail.ru>
2020-12-24 10:48:30 +0300

    Lesson 05 HW04

--- src/test/java/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ src/test/java/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -1,12 +1,9 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+public class ArrayStorageTest extends AbstractArrayStorageTest {
 
-public class ArrayStorageTest extends AbstractArrayStorageTest{
-
-    @BeforeClass
-        public static void init() {
-        }
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
 
 }
