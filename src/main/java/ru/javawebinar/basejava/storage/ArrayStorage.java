1e267c006415936416749057870c9c911b1f4eb0
vitov <vitovt.kublitsky@mail.ru>
2020-12-24 17:40:53 +0300

    Lesson05 Static initializer block and Collections

--- src/main/java/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/main/java/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -2,8 +2,6 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
@@ -11,16 +9,21 @@ public class ArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void fillDeletedElement(int index) {
-
+        storage[index] = storage[size - 1];
     }
 
     @Override
     protected void insertElement(Resume r, int index) {
-
+        storage[size] = r;
     }
 
     @Override
     protected int getIndex(String uuid) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
