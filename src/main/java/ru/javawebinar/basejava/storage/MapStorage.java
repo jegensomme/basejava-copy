f87eb13f70456908224014a63588a52e8bbde767
vitov <vitovt.kublitsky@mail.ru>
2021-01-23 16:12:13 +0300

    HW5 MapStorage realization

---
+++ src/main/java/ru/javawebinar/basejava/storage/MapStorage.java
@@ -0,0 +1,55 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+    private Map<String, Resume> map = new HashMap<>();
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return uuid;
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return map.containsKey(searchKey);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object searchKey) {
+        map.put((String) searchKey, r);
+    }
+
+    @Override
+    protected void doSave(Resume r, Object searchKey) {
+        map.put((String) searchKey, r);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return map.get(searchKey);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        map.remove(searchKey);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return map.values().toArray(new Resume[map.size()]);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+}
